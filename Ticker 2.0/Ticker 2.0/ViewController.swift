e0fde84c8fd108920e705cca8ab3f52432424f92
Jackson Tubbs <decknot@jacksons-mbp.attlocal.net>
2020-01-14 20:03:49 -0800

    Initial Commit

---
+++ Ticker 2.0/Ticker 2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ticker 2.0
+//
+//  Created by Jackson Tubbs on 1/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
